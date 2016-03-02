c95a1ce635d1aed22a0a544c254a32c36314f1ea
carsencoggeshall <carsen.coggeshall@pinecrest.edu>
2016-03-02 15:53:16 -0500

    Update LetMeSee.py

--- Intro/5_LetMeSee/LetMeSee.py
+++ Intro/5_LetMeSee/LetMeSee.py
@@ -64,11 +64,11 @@ class MPCR_Rover_Image(Rover20):
         self.currentImage = cv2.imdecode(np.asarray(bytearray(jpegbytes), dtype=np.uint8), 0)
 
         self.pattern = np.reshape(self.currentImage, (240 * 320))
-
+        # Ask the user to enter a number for a specific action. The number entered is now equal to self.action_choice
         self.action_choice = input("Enter 1 for left, 2 for forward, 3 for right, 4 for reverse, 5 for save and quit)")
-
-        self.action_choice = self.action_choice - 1
-
+        
+        self.action_choice = self.action_choice - 1  # Subtract 1 from the value assigned to self.action_choice
+                            # this makes the value entered align with the value required for the operation (seen in lines ~72, 90-100)
         if self.action_choice == 4:
             datasave = {}
             datasave['data'] = self.data
@@ -89,13 +89,13 @@ class MPCR_Rover_Image(Rover20):
 
         print np.asarray(self.data).shape[1]
 
-        if self.action_choice == 0:
-            self.mleft()
-        elif self.action_choice == 1:
-            self.mforward()
-        elif self.action_choice == 2:
-            self.mright()
-        elif self.action_choice == 3:
+        if self.action_choice == 0:    # If self.action_choice equals 0, run self.mleft(). We've already defined what that is,
+            self.mleft()               # so the rover turns left. If it doesn't equal 0, check the next "elif"
+        elif self.action_choice == 1:  # If self.action_choice equals 1, move the rover forward according 
+            self.mforward()            # to the self.mforward() function. If it doesn't, check the next "elif".
+        elif self.action_choice == 2:  # If self.action_choice equals 2, turn the rover right.
+            self.mright()              # If it doesn't, move on and check the next "elif"
+        elif self.action_choice == 3:  # If self.action_choice equals 3, move the rover backward.
             self.mbackward()
 
 
